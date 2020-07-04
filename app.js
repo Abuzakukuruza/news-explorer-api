720ead14f13b774d2f3e5e3b9750b3fab529ef70
Danil Plyusnin <dplusnin0@gmail.com>
2020-07-04 15:40:26 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const cors = require('cors');
+
+const { PORT, mongooseConfig, MONGOOSE_URL } = require('./config/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const BadRequstError = require('./errors/bad-request-err');
+const routerMain = require('./routes/index');
+const auth = require('./middlewares/auth');
+const errorsHandler = require('./middlewares/errors-handler');
+const { createUser, login } = require('./controllers/users');
+
+
+const app = express();
+
+
+mongoose.connect(MONGOOSE_URL, mongooseConfig);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+// подключение логгера запросов
+app.use(requestLogger);
+
+// краш-тест
+// app.get('/crash-test', () => {
+//     setTimeout(() => {
+//         throw new BadGatewayError('Сервер сейчас упадёт');
+//     }, 0);
+// });
+
+// роуты регистрации и логина
+
+const corsOptions = {
+    origin: [
+        'http://localhost:8080',
+        'https://lsnsky.github.io/'
+    ],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.post('/signup', celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required().min(2).max(30),
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8),
+    }),
+}), createUser);
+
+app.post('/signin', celebrate({
+    body: Joi.object().keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8),
+    }),
+}), login);
+
+// авторизация, дальнейшие роуты защищены
+app.use(auth);
+
+// роуты доступа к информации
+app.use('/', routerMain);
+
+// подключение логгера ошибок
+app.use(errorLogger);
+
+// обработчики ошибок
+
+// ошибка при неправильном адресе в строке
+app.use('*', () => {
+    throw new BadRequstError('Запрашиваемый ресурс не найден');
+});
+
+// обработчик ошибок celebrate
+app.use(errors());
+
+
+// централизованный обработчик ошибок
+app.use(errorsHandler);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { getUserInfo } = require('../controllers/users');
+
+
+router.get('/me', getUserInfo);
+
+
+module.exports = router;
